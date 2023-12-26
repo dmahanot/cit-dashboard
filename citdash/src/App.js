0176f0318de7b9981df95cd6748c2ca8c59b64d8
dmahanot <mdiksha.netapp.com>
2023-12-26 12:40:28 +0530

    dashboard layout

--- citdash/src/App.js
+++ citdash/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
-    <>
-    <Login />
+     <>
+    {/* <Login /> */}
+    <Dashboard />
     </>
   )
 }
