6cae1d5f47264d6fee1293fa9c1058bad60e035e
dmahanot <mdiksha.netapp.com>
2023-12-30 16:23:06 +0530

    Jenkins_new page fix

--- citdash/src/App.js
+++ citdash/src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
-import Jenkins from './components/jenkins_execution/Jenkins';
+import Jenkins_new from './components/jenkins_execution/Jenkins_new';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
       {/* <Login /> */}
-      <Navbar />
+      
       <Routes>
         <Route path="dashboard" element={<Dashboard />} />
-        <Route path="jenkins" element={<Jenkins />} />
+        <Route path="jenkins_new" element={<Jenkins_new />} />
       </Routes>
     </>
   )
